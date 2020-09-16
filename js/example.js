8084f561688cd7e4ea49d4fec5a2014bad92ca5a
teenoy2542 <56290521+teenoy2542@users.noreply.github.com>
2020-09-16 13:38:00 +0700

    [Update]
    +example

--- js/example.js
+++ js/example.js
@@ -1,1 +1,19 @@
-console.log("hello wolrd")
+const Pi = 3.14
+console.log("Pi : ",Pi)
+
+let name = "john"
+name = `${Pi} jame`
+console.log(name)
+
+let arr = ['apple','banana']
+console.log("arr :",arr[0])
+
+let person = {
+    name: 'joke',
+    age: 15,
+    gender: {
+        name: "male",        
+    },
+    arr
+}
+console.log("Obj : ",person)
