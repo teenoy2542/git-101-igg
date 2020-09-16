4c1c877531df661ce8a20b39aae6bba0f7d94ab9
teenoy2542 <56290521+teenoy2542@users.noreply.github.com>
2020-09-16 15:36:53 +0700

    [Add]
    +quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,8 @@
+var word1 = "Hello my name is joke"
+console.log(word1.length)
+
+var word2 = "Hello my name is Joke nice to meet you!"
+console.log(word2.indexOf("Joke"))
+
+var word3 = "Well open up your mind and see like me"
+console.log(word3.slice(5,9))
