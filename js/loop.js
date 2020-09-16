5cb606a9d9f5db683332ce1a092dc3872005caeb
teenoy2542 <56290521+teenoy2542@users.noreply.github.com>
2020-09-16 14:38:50 +0700

    [Update] loop
    +fillter()

--- js/loop.js
+++ js/loop.js
@@ -13,9 +13,9 @@
 
 // arr.map((item,index) => {console.log(item,index)})
 
-for(let i=1; i<=20; i+=1){
-    console.log(i)
-}
+// for(let i=1; i<=20; i+=1){
+//     console.log(i)
+// }
 
 let temp = ['apple','banana','kiwi']
 // temp.forEach((Element,idx) => {
@@ -26,3 +26,6 @@ let newValue = temp.map((Element,idx) => {
     return `${idx+1}.${Element}`
 })
 console.log(newValue)
+
+let fillterMoreThanZero = [-1,2,3,56,6,-93,0,4].filter(el => el >=0)
+console.log(fillterMoreThanZero)
