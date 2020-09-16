82cb06f42e3a931597662182ce52f3c2631e1015
teenoy2542 <56290521+teenoy2542@users.noreply.github.com>
2020-09-16 15:54:17 +0700

    [Add]
    +syncAndAsync

---
+++ js/syncAndAsync.js
@@ -0,0 +1,24 @@
+// console.log("1")
+// console.log("2")
+// new Promise((resolve,reject) => {
+//     setTimeout(()=>{
+//         resolve(console.log("3"))
+//     }, 1000);
+// }).then(() => {
+//     console.log("4")
+//     console.log("5")
+// })
+
+myFirstAsync = () => {
+    return new Promise(resolve =>{
+        setTimeout(()=>{
+            resolve('Success');
+        },250);
+    })
+}
+
+msg = async () => {
+    const successMessage = await myFirstAsync();
+    console.log('yay! ',successMessage);
+}
+msg();
